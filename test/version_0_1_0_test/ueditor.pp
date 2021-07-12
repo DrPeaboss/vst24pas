2ec04dfc4d6de93104f2e03b4512aef56093c8d4
Peazomboss <qtzx170301@163.com>
2021-07-13 00:06:26 +0800

    Fix error forgot in last commit and add some new tests.

--- test/version_0_1_0_test/ueditor.pp
+++ test/version_0_1_0_test/ueditor.pp
@@ -5,21 +5,30 @@ unit ueditor;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, ExtCtrls, StdCtrls,
+  umain;
 
 type
 
   { TFormMain }
 
   TFormMain = class(TForm)
+    LabelGain: TLabel;
+    LabelInput: TLabel;
+    LabelInputShowDelay: TLabel;
+    LabelOutput: TLabel;
     Timer: TTimer;
     TrackBarGain: TTrackBar;
+    TrackBarInputDelay: TTrackBar;
+    procedure FormCreate(Sender: TObject);
     procedure TimerTimer(Sender: TObject);
     procedure TrackBarGainChange(Sender: TObject);
+    procedure TrackBarInputDelayChange(Sender: TObject);
   private
     FLocked:boolean;
+    Gain:single;
   public
-
+    Plugin:TNewGain;
   end;
 
 implementation
@@ -33,15 +42,30 @@ uses
 
 procedure TFormMain.TrackBarGainChange(Sender: TObject);
 begin
+  Gain:=TrackBarGain.Position/2000;
   if not FLocked then
-    gPlugin.SetParameterAutomated(0,TrackBarGain.Position/2000);
+    Plugin.SetParameterAutomated(0,Gain);
+  LabelGain.Caption := Format('Gain %.3fdB %.3f',[VstAmp2dB(Gain),Gain]);
+end;
+
+procedure TFormMain.TrackBarInputDelayChange(Sender: TObject);
+begin
+  LabelInputShowDelay.Caption := Format('ShowDelay %d ms',[TrackBarInputDelay.Position*10]);
 end;
 
 procedure TFormMain.TimerTimer(Sender: TObject);
 begin
+  Gain:=Plugin.GetParameter(0)*2;
   FLocked:=True;
-  TrackBarGain.Position := Round(gPlugin.GetParameter(0)*2000);
+  TrackBarGain.Position := Round(Gain*1000);
+  LabelInput.Caption := Format('Input %.3fdB %.3f',[VstAmp2dB(Plugin.Peak),Plugin.Peak]);
+  LabelOutput.Caption := Format('Output %.3fdB %.3f',[VstAmp2dB(Plugin.Peak*Gain),Plugin.Peak*Gain]);
   FLocked:=False;
+end;
+
+procedure TFormMain.FormCreate(Sender: TObject);
+begin
+  LabelGain.Caption := Format('Gain %.3fdB %.3f',[VstAmp2dB(1.0),1.0]);
 end;
 
 end.
--- test/version_0_1_0_test/umain.pp
+++ test/version_0_1_0_test/umain.pp
@@ -12,16 +12,22 @@ type
   { TNewGain }
 
   TNewGain = class(TPluginBase)
+  private
+    FTick:QWord;
+    usebuf1:boolean;
+    buf1,buf2:single;
+    FPeak:single;
   public
     constructor Create(VstHost: TVstHostCallback; NumPrograms, NumParams: Int32); override;
     procedure ProcessReplacing(const Inputs, Outputs: TBuffer32; SampleFrames: Int32); override;
     function CustomDisplay(index:int32):string;
+    property Peak:single read FPeak;
   end;
 
 implementation
 
 uses
-  ueditor,sysutils,vst24pas.utils;
+  ueditor,sysutils,vst24pas.utils,vst24pas.gui,math;
 
 { TNewGain }
 
@@ -35,6 +41,7 @@ begin
   PlugInitProgramInfo(1,'Program 0: silent',[0]);
   PlugInitProgramInfo(2,'Program 2: double',[1]);
   OnCustomDisplay := @CustomDisplay;
+  TFormMain(TGuiEditor(FEditor).Gui).Plugin:=self;
 end;
 
 procedure TNewGain.ProcessReplacing(const Inputs, Outputs: TBuffer32; SampleFrames: Int32);
@@ -42,11 +49,23 @@ var
   i: Integer;
   v: single;
 begin
+  if GetTickCount64-FTick>TFormMain(TGuiEditor(FEditor).Gui).TrackBarInputDelay.Position*10 then
+  begin
+    if usebuf1 then FPeak := buf1 else FPeak := buf2;
+    buf1:=0;
+    buf2:=0;
+    usebuf1:=not usebuf1;
+    FTick:=GetTickCount64;
+  end;
   v:= 2*ParamInfos[0].Value;
   for i:=0 to SampleFrames-1 do
   begin
     Outputs[0,i]:=Inputs[0,i]*v;
     Outputs[1,i]:=Inputs[1,i]*v;
+    if usebuf1 then
+      buf1:=max(buf1,0.5*(Inputs[0,i]+Inputs[1,i]))
+    else
+      buf2:=max(buf2,0.5*(Inputs[0,i]+Inputs[1,i]));
   end;
 end;
 
