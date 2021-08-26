4cf6a962eaa83159a4e197a02a7671931e295426
Peazomboss <qtzx170301@163.com>
2021-08-27 01:57:55 +0800

    Add oscillator without MIDI

--- test/version_0_2_x_tests/v02xtest3editor.pp
+++ test/version_0_2_x_tests/v02xtest3editor.pp
@@ -5,17 +5,31 @@ unit v02xtest3editor;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs,StdCtrls,v02xtest3main;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs,StdCtrls,ExtCtrls,ComCtrls,v02xtest3main;
 
 type
 
   { TFormain }
 
   TFormain = class(TForm)
-    BtnGetDir:TButton;
-    procedure BtnGetDirClick(Sender:TObject);
+    BtnPlayAShot:TButton;
+    lblFreq:TLabel;
+    lblGain:TLabel;
+    rgWav:TRadioGroup;
+    tbFreq:TTrackBar;
+    tbGain:TTrackBar;
+    tbPlayPause:TToggleBox;
+    Timer:TTimer;
+    procedure BtnPlayAShotClick(Sender:TObject);
+    procedure FormCreate(Sender:TObject);
+    procedure rgWavClick(Sender:TObject);
+    procedure tbFreqChange(Sender:TObject);
+    procedure tbGainChange(Sender:TObject);
+    procedure tbPlayPauseChange(Sender:TObject);
+    procedure TimerTimer(Sender:TObject);
   private
-
+    FCount:Integer;
+    FPersistentPlaying:Boolean;
   public
     Plugin:TV02XTest3;
   end;
@@ -24,18 +38,57 @@ type
 implementation
 
 uses
-  vst2intf;
+  vst2intf,oscillator;
 
 {$R *.lfm}
 
 { TFormain }
 
-procedure TFormain.BtnGetDirClick(Sender:TObject);
-var
-  curdir:String;
+procedure TFormain.rgWavClick(Sender:TObject);
 begin
-  curdir:=strpas(FromIntPtr(Plugin.Base.CallHost(amGetDirectory)));
-  ShowMessage(curdir);
+  Plugin.Osc.OscMode:=TOscModes(rgWav.ItemIndex);
+end;
+
+procedure TFormain.tbFreqChange(Sender:TObject);
+begin
+  lblFreq.Caption:=Format('Frequency: %d HZ',[tbFreq.Position]);
+  with Plugin.Osc do Freq:=tbFreq.Position;
+end;
+
+procedure TFormain.tbGainChange(Sender:TObject);
+var
+  gain:Double;
+begin
+  gain:=tbGain.Position/10000;
+  lblGain.Caption:=Format('Gain: %.3f dB',[VstAmp2dB(gain)]);
+  Plugin.Osc.Gain:=gain;
+end;
+
+procedure TFormain.tbPlayPauseChange(Sender:TObject);
+begin
+  with Plugin.Osc do IsMuted:=not tbPlayPause.Checked;
+  FPersistentPlaying:=tbPlayPause.Checked;
+end;
+
+procedure TFormain.BtnPlayAShotClick(Sender:TObject);
+begin
+  Plugin.Osc.IsMuted:=False;
+  FCount:=0;
+end;
+
+procedure TFormain.FormCreate(Sender:TObject);
+begin
+  lblFreq.Caption:='Frequency: 440 HZ';
+  lblGain.Caption:='Gain: 0.000 dB';
+end;
+
+procedure TFormain.TimerTimer(Sender:TObject);
+begin
+  rgWav.ItemIndex:=Ord(Plugin.Osc.OscMode);
+  Inc(Fcount);
+  if FPersistentPlaying then Exit;
+  with Plugin.Osc do
+  if (FCount>30) then IsMuted:=True;
 end;
 
 end.
--- test/version_0_2_x_tests/v02xtest3main.pp
+++ test/version_0_2_x_tests/v02xtest3main.pp
@@ -1,3 +1,8 @@
+{
+Thanks to
+http://www.martin-finke.de/blog/articles/audio-plugins-008-synthesizing-waveforms/
+}
+
 unit v02xtest3main;
 
 {$mode ObjFPC}{$H+}
@@ -5,15 +10,21 @@ unit v02xtest3main;
 interface
 
 uses
-  vst2intf,vst2plugui;
+  vst2intf,vst2plugui,oscillator;
 
 type
-
   { TV02XTest3 }
 
   TV02XTest3 = class(TVGuiPlugin)
+  private
+    FOsc:TSingleOsc;
+  protected
+    function Dispatcher(opcode:TAEOpcodes;index:Int32;value:IntPtr;ptr:Pointer;opt:Single):IntPtr;override;
+    procedure ProcessRep(const inputs,outputs:TBuffer32;SampleFrames:Int32);override;
   public
     constructor Create(AHost:THostCallback);override;
+    destructor Destroy;override;
+    property Osc:TSingleOsc read FOsc;
   end;
 
 implementation
@@ -23,14 +34,37 @@ uses
 
 { TV02XTest3 }
 
+function TV02XTest3.Dispatcher(opcode:TAEOpcodes;index:Int32;value:IntPtr;ptr:Pointer;opt:Single):IntPtr;
+begin
+  if opcode=effSetSampleRate then FOsc.SampleRate:=opt;
+  Result:=inherited Dispatcher(opcode,index,value,ptr,opt);
+end;
+
+procedure TV02XTest3.ProcessRep(const inputs,outputs:TBuffer32;SampleFrames:Int32);
+var
+  i:Integer;
+begin
+  FOsc.Generage(outputs[0],SampleFrames);
+  for i:=0 to SampleFrames-1 do
+    outputs[1][i]:=outputs[0][i];
+end;
+
 constructor TV02XTest3.Create(AHost:THostCallback);
 begin
   inherited Create(AHost);
   Base.SetUniqueID('PZn5');
   Base.SetNames('v02xtest3','PeaZomboss','vst24pas: v02xtest3');
   Base.SetVersion($00020005,$00020005);
+  Base.SetAsSynth;
   Editor.SetGui(TFormain);
   TFormain(Editor.Gui).Plugin:=self;
+  FOsc:=TSingleOsc.Create(omTriangle);
+end;
+
+destructor TV02XTest3.Destroy;
+begin
+  FOsc.Free;
+  inherited Destroy;
 end;
 
 end.
